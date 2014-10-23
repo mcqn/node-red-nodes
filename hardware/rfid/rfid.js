0fc1560b189149bd8f2d89f41e387620807b6502
neil winterburn <neilwinterburn@gmail.com>
2014-10-23 16:45:45 +0100

    Added new node to write blocks to an RFID tag.

--- hardware/rfid/rfid.js
+++ hardware/rfid/rfid.js
@@ -16,8 +16,11 @@
 
 module.exports = function(RED) {
     "use strict";
-    var rfid_sl018 = require("rfid-sl018");
+    var rfid_sl030 = require("rfid-sl030");
     var fs =  require('fs');
+    // Create a single RFID instance that all nodes can use
+    var ourfid = new rfid_sl030.RFID_SL030();
+    ourfid.init();
 
     if (!fs.existsSync("/dev/ttyAMA0")) { // unlikely if not on a Pi
         throw "Info : Ignoring Raspberry Pi specific node.";
@@ -27,8 +30,7 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,n);
         this.cardPresent = false;
         this.cardID = "";
-        this.rfid = new rfid_sl018.RFID_SL018();
-        this.rfid.init();
+        this.rfid = ourfid; 
         var node = this;
 
         node._interval = setInterval( function() {
@@ -57,19 +59,47 @@ module.exports = function(RED) {
         });
     }
 
+    function RFIDWrite(n) {
+        RED.nodes.createNode(this,n);
+        this.name = n.name;
+        this.rfid = ourfid; 
+        var node = this;
 
-    //exec("gpio mode 0 in",function(err,stdout,stderr) {
-    //    if (err) {
-    //        util.log('[36-rpi-gpio.js] Error: "gpio" command failed for some reason.');
-    //    }
-    //    exec("gpio mode 1 in");
-    //    exec("gpio mode 2 in");
-    //    exec("gpio mode 3 in");
-    //    exec("gpio mode 4 in");
-    //    exec("gpio mode 5 in");
-    //    exec("gpio mode 6 in");
-    //    exec("gpio mode 7 in");
-    //});
+        this.on("input", function(msg) {
+            if (msg != null) {
+                if (msg.block != null && msg.payload) {
+                    // We've got our pre-requisites
+                    // Find an RFID tag first
+                    var tag = this.rfid.selectTag();
+                    if (tag) {
+                        // Tag found.  Authenticate with the block first
+                        if (this.rfid.authenticate(this.rfid.sectorForBlock(msg.block))) {
+                            // Authenticated.  Prepare the data to write
+                            // Blocks on the MiFare 1K tags are 16 bytes long
+                            var data = new Buffer(16);
+                            data.fill(0);
+                            data.write(msg.payload);
+                            //console.log("About to write '"+msg.payload+"' to "+msg.block);
+                            if (this.rfid.writeBlock(msg.block, data)) {
+                                this.send(msg);
+                            } else {
+                                this.error("Failed to write to RFID tag");
+                            }
+                        } else {
+                            // Error, couldn't authenticate tag
+                            this.error("Couldn't authenticate RFID tag");
+                        }
+                    } else {
+                        // Failed to find a tag
+                        this.error("No RFID tag found");
+                    }
+                } else {
+                    this.error("Missing either a msg.block or a msg.payload");
+                }
+            }
+        });
+    }
 
     RED.nodes.registerType("rpi-rfid in",RFID);
+    RED.nodes.registerType("rpi-rfid write",RFIDWrite);
 }
